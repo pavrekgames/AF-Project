a1e542424fd29c3301fd8634c3f2cb1d8de089d4
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-22 18:09:58 +0100

    Implement receiving damage and dead of unit

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -13,5 +13,26 @@ namespace AFSInterview.Units
         [SerializeField] protected int attackDamage;
         [SerializeField] protected int optionalAttackDamage;
 
+
+        protected virtual void Attack(int damage)
+        {
+
+        }
+
+        public void ReceiveDamage(int damage)
+        {
+            int damagePoint = damage - armor;
+            health -= damagePoint;
+            CheckUnitDead();
+        }
+
+        protected virtual void CheckUnitDead()
+        {
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
     }
 }
