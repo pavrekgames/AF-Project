f75c44b6d343d920c656c44b3d33229d42bcfd44
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-22 17:44:55 +0100

    Add basic variables to Unit abstract class

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -6,7 +6,11 @@ namespace AFSInterview.Units
 {
     public abstract class Unit : MonoBehaviour
     {
-        
+        [SerializeField] protected int health;
+        [SerializeField] protected int armor;
+        [SerializeField] protected int attackInterval;
+        [SerializeField] protected int attackDamage;
+        [SerializeField] protected int optionalAttackDamage;
 
     }
 }
