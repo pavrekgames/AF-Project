bea6d9b40e596eabfc990e35e6055a502168084f
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-22 20:12:02 +0100

    Create CombatManager script

---
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AFSInterview.Combat
+{
+    public class CombatManager : MonoBehaviour
+    {
+        
+
+
+    }
+}
