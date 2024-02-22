0cc29e5148f66ad64d5789606d1838c9da916b57
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-22 12:59:38 +0100

    Fix SellAllItemsUpToValue method in InventoryController

--- Assets/Scripts/Items/InventoryController.cs
+++ Assets/Scripts/Items/InventoryController.cs
@@ -1,32 +1,59 @@
 ﻿namespace AFSInterview.Items
 {
-	using System.Collections.Generic;
-	using UnityEngine;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-	public class InventoryController : MonoBehaviour
-	{
-		[SerializeField] private List<Item> items;
-		[SerializeField] private int money;
+    public class InventoryController : MonoBehaviour
+    {
+        [SerializeField] private List<Item> items;
+        [SerializeField] private int money;
 
-		public int Money => money;
-		public int ItemsCount => items.Count;
+        public int Money => money;
+        public int ItemsCount => items.Count;
 
-		public void SellAllItemsUpToValue(int maxValue)
-		{
-			for (var i = 0; i < items.Count; i++)
-			{
-				var itemValue = items[i].Value;
-				if (itemValue > maxValue)
-					continue;
-				
-				money += itemValue;
-				items.RemoveAt(i);
-			}
-		}
+        public void SellAllItemsUpToValue(int maxValue)
+        {
+            int itemsValue = 0;
+            List<Item> itemsToRemove = new List<Item>();
 
-		public void AddItem(Item item)
-		{
-			items.Add(item);
-		}
-	}
+            for (var i = 0; i < items.Count; i++)
+            {
+                if (itemsValue < maxValue)
+                {
+                    itemsValue += items[i].Value;
+
+                    if (itemsValue <= maxValue)
+                    {
+                        money += items[i].Value;
+                        itemsToRemove.Add(items[i]);
+                        continue;
+                    }
+                    else
+                    {
+                        itemsValue -= items[i].Value;
+                        continue;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            RemoveItemsUpToValue(items, itemsToRemove);
+        }
+
+        private void RemoveItemsUpToValue(List<Item> items, List<Item> itemsToRemove)
+        {
+            foreach (var item in itemsToRemove)
+            {
+                items.Remove(item);
+            }
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+        }
+    }
 }
