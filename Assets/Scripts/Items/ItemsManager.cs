0319ffe61fc5f0d2dc5d0d2be5e88683e93b8904
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-22 13:22:19 +0100

    Refactor using camera ray to pick up items in ItemsManager

--- Assets/Scripts/Items/ItemsManager.cs
+++ Assets/Scripts/Items/ItemsManager.cs
@@ -1,56 +1,68 @@
 ﻿namespace AFSInterview.Items
 {
-	using TMPro;
-	using UnityEngine;
+    using TMPro;
+    using UnityEngine;
 
-	public class ItemsManager : MonoBehaviour
-	{
-		[SerializeField] private InventoryController inventoryController;
-		[SerializeField] private int itemSellMaxValue;
-		[SerializeField] private Transform itemSpawnParent;
-		[SerializeField] private GameObject itemPrefab;
-		[SerializeField] private BoxCollider itemSpawnArea;
-		[SerializeField] private float itemSpawnInterval;
+    public class ItemsManager : MonoBehaviour
+    {
+        [SerializeField] private InventoryController inventoryController;
+        [SerializeField] private int itemSellMaxValue;
+        [SerializeField] private Transform itemSpawnParent;
+        [SerializeField] private GameObject itemPrefab;
+        [SerializeField] private BoxCollider itemSpawnArea;
+        [SerializeField] private float itemSpawnInterval;
+
+        [Header("Camera Ray")]
+        private Camera mainCamera;
+        private LayerMask layerMask;
+
+        private void Initialize()
+        {
+            mainCamera = Camera.main;
+            layerMask = LayerMask.GetMask("Item");
+        }
 
         private void Start()
         {
+            Initialize();
+
             InvokeRepeating("SpawnNewItem", 0, itemSpawnInterval);
         }
 
         private void Update()
-		{
+        {
 
-			if (Input.GetMouseButtonDown(0))
-				TryPickUpItem();
-			
-			if (Input.GetKeyDown(KeyCode.Space))
-				inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
+            if (Input.GetMouseButtonDown(0))
+                TryPickUpItem();
 
-			FindObjectOfType<TextMeshProUGUI>().text = "Money: " + inventoryController.Money;
-		}
+            if (Input.GetKeyDown(KeyCode.Space))
+                inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
 
-		private void SpawnNewItem()
-		{
-			var spawnAreaBounds = itemSpawnArea.bounds;
-			var position = new Vector3(
-				Random.Range(spawnAreaBounds.min.x, spawnAreaBounds.max.x),
-				0f,
-				Random.Range(spawnAreaBounds.min.z, spawnAreaBounds.max.z)
-			);
-			
-			Instantiate(itemPrefab, position, Quaternion.identity, itemSpawnParent);
-		}
+            FindObjectOfType<TextMeshProUGUI>().text = "Money: " + inventoryController.Money;
+        }
 
-		private void TryPickUpItem()
-		{
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			var layerMask = LayerMask.GetMask("Item");
-			if (!Physics.Raycast(ray, out var hit, 100f, layerMask) || !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder))
-				return;
-			
-			var item = itemHolder.GetItem(true);
+        private void SpawnNewItem()
+        {
+            var spawnAreaBounds = itemSpawnArea.bounds;
+            var position = new Vector3(
+                Random.Range(spawnAreaBounds.min.x, spawnAreaBounds.max.x),
+                0f,
+                Random.Range(spawnAreaBounds.min.z, spawnAreaBounds.max.z)
+            );
+
+            Instantiate(itemPrefab, position, Quaternion.identity, itemSpawnParent);
+        }
+
+        private void TryPickUpItem()
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (!Physics.Raycast(ray, out var hit, 100f, layerMask) || !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder))
+                return;
+
+            var item = itemHolder.GetItem(true);
             inventoryController.AddItem(item);
             Debug.Log("Picked up " + item.Name + " with value of " + item.Value + " and now have " + inventoryController.ItemsCount + " items");
-		}
-	}
+        }
+    }
 }
